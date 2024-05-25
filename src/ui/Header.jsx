169a0b7bde287f51328642f92e872054e966a2c2
okharyton <olegkhacv@gmail.com>
2024-05-25 21:11:20 +0300

    feat: add test tailwind styles

--- src/ui/Header.jsx
+++ src/ui/Header.jsx
@@ -1,14 +1,17 @@
 import { Link } from "react-router-dom";
 import SearchOrder from "../features/order/SearchOrder.jsx";
+import Username from "../features/user/Username.jsx";
 
 export default function Header() {
   return (
-    <header>
-      <Link to="/">Fast React Pizza Co.</Link>
+    <header className="bg-yellow-500 uppercase">
+      <Link to="/" className="tracking-widest">
+        Fast React Pizza Co.
+      </Link>
 
       <SearchOrder />
 
-      <p>John Doe</p>
+      <Username />
     </header>
   );
 }
