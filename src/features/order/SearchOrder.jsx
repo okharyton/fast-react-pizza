fc6cddda9d221c512b548c3eec810db95ca68c9d
okharyton <olegkhacv@gmail.com>
2024-05-29 22:21:11 +0300

    feat: add styles to inputs

--- src/features/order/SearchOrder.jsx
+++ src/features/order/SearchOrder.jsx
@@ -12,6 +12,7 @@ export default function SearchOrder() {
   return (
     <form onSubmit={handleSubmit}>
       <input
+        className="rounded-full bg-yellow-100 px-4 py-2 text-sm placeholder:text-stone-400 sm:focus:w-72 sm:w-64 transition-all duration-300 focus:outline-none focus:ring focus:ring-yellow-500 focus:ring-opacity-50"
         placeholder="Search Order #"
         value={query}
         onChange={(e) => setQuery(e.target.value)}
