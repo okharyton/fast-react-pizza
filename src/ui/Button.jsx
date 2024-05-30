99118bf386bd81fc2d5c6f697c31d9c22b0513af
okharyton <olegkhacv@gmail.com>
2024-05-30 19:49:24 +0300

    feat: add styles to menu

--- src/ui/Button.jsx
+++ src/ui/Button.jsx
@@ -1,16 +1,21 @@
 import { Link } from "react-router-dom";
 
-export default function Button({ children, disabled, to }) {
-  const className =
-    "sm:px-6 sm:py-4 bg-yellow-400 uppercase font-semibold text-stone-800 py-3 px-3 inline-block tracking-wide rounded-full hover:bg-yellow-300 transition-colors duration-300 focus:outline-none focus:ring focus:ring-yellow-300 focus:bg-yellow-300 focus:ring-offset-2 cursor-not-allowed";
+export default function Button({ children, disabled, to, type }) {
+  const base =
+    "bg-yellow-400 uppercase font-semibold text-stone-800 inline-block tracking-wide rounded-full hover:bg-yellow-300 transition-colors duration-300 focus:outline-none focus:ring focus:ring-yellow-300 focus:bg-yellow-300 focus:ring-offset-2 cursor-not-allowed";
+
+  const styles = {
+    primary: base + " md:px-6 md:py-4 py-3 px-4",
+    small: base + " px-4 py-2 md:px-5 md:py-2.5 text-xs",
+  };
   if (to)
     return (
-      <Link to={to} className={className}>
+      <Link to={to} className={styles[type]}>
         {children}
       </Link>
     );
   return (
-    <button className={className} disabled={disabled}>
+    <button className={styles[type]} disabled={disabled}>
       {children}
     </button>
   );
