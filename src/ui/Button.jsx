ebd4aea0fd883f08fdb17684d02bbfb60cd3acde
okharyton <olegkhacv@gmail.com>
2024-05-30 21:46:36 +0300

    feat: add styles to order form

--- src/ui/Button.jsx
+++ src/ui/Button.jsx
@@ -2,13 +2,13 @@ import { Link } from "react-router-dom";
 
 export default function Button({ children, disabled, to, type }) {
   const base =
-    "bg-yellow-400 uppercase font-semibold text-stone-800 inline-block tracking-wide rounded-full hover:bg-yellow-300 transition-colors duration-300 focus:outline-none focus:ring focus:ring-yellow-300 focus:bg-yellow-300 focus:ring-offset-2 cursor-not-allowed";
+    "bg-yellow-400 uppercase text-sm font-semibold text-stone-800 inline-block tracking-wide rounded-full hover:bg-yellow-300 transition-colors duration-300 focus:outline-none focus:ring focus:ring-yellow-300 focus:bg-yellow-300 focus:ring-offset-2 cursor-not-allowed";
 
   const styles = {
     small: base + " px-4 py-2 md:px-5 md:py-2.5 text-xs",
     primary: base + " md:px-6 md:py-4 py-3 px-4",
     secondary:
-      "border-2 border-stone-300 md:px-6 md:py-3.5 py-2.5 px-4 uppercase font-semibold text-stone-400 inline-block tracking-wide rounded-full hover:bg-stone-300 hover:text-stone-800 transition-colors duration-300 focus:outline-none focus:ring focus:ring-stone-200 focus:bg-stone-300 focus:text-stone-800 focus:ring-offset-2 cursor-not-allowed",
+      "text-sm border-2 border-stone-300 md:px-6 md:py-3.5 py-2.5 px-4 uppercase font-semibold text-stone-400 inline-block tracking-wide rounded-full hover:bg-stone-300 hover:text-stone-800 transition-colors duration-300 focus:outline-none focus:ring focus:ring-stone-200 focus:bg-stone-300 focus:text-stone-800 focus:ring-offset-2 cursor-not-allowed",
   };
   if (to)
     return (
