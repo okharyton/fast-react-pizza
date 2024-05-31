88c98a4dfcc603ff7d897b93cc87a21263520b8f
okharyton <olegkhacv@gmail.com>
2024-05-31 23:04:40 +0300

    feat: modeling the cart state

--- src/features/cart/Cart.jsx
+++ src/features/cart/Cart.jsx
@@ -1,6 +1,7 @@
 import LinkButton from "../../ui/LinkButton.jsx";
 import Button from "../../ui/Button.jsx";
 import CartItem from "./CartItem.jsx";
+import { useSelector } from "react-redux";
 
 const fakeCart = [
   {
@@ -27,13 +28,14 @@ const fakeCart = [
 ];
 
 function Cart() {
+  const username = useSelector((state) => state.user.username);
   const cart = fakeCart;
 
   return (
     <div className="py-3 px-4">
       <LinkButton to="/menu">&larr; Back to menu</LinkButton>
 
-      <h2 className="mt-7 text-xl font-semibold">Your cart, %NAME%</h2>
+      <h2 className="mt-7 text-xl font-semibold">Your cart, {username}</h2>
 
       <ul className="divide-y divide-stone-200 border-b mt-3">
         {cart.map((item) => (
