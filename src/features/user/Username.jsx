cd2be1a63761e58d40233b8810cdd496a93ece0e
okharyton <olegkhacv@gmail.com>
2024-05-31 22:39:59 +0300

    feat: reading & updating the user state

--- src/features/user/Username.jsx
+++ src/features/user/Username.jsx
@@ -1,3 +1,9 @@
+import { useSelector } from "react-redux";
+
 export default function Username() {
-  return <div className="text-sm font-semibold hidden md:block">Oleg</div>;
+  const username = useSelector((state) => state.user.username);
+  if (!username) return null;
+  return (
+    <div className="text-sm font-semibold hidden md:block">{username}</div>
+  );
 }
