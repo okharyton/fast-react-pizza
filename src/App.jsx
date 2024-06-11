c34573ee8be0f7f752d4e49d3e1a9da46c94d25f
okharyton <olegkhacv@gmail.com>
2024-06-11 17:42:00 +0300

    feat: updating data without navigation

--- src/App.jsx
+++ src/App.jsx
@@ -3,10 +3,13 @@ import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import Home from "./ui/Home.jsx";
 import Menu, { loader as menuLoader } from "./features/menu/Menu.jsx";
 import Cart from "./features/cart/Cart.jsx";
-import CreateOrder, {action as createOrderAction} from "./features/order/CreateOrder.jsx";
+import CreateOrder, {
+  action as createOrderAction,
+} from "./features/order/CreateOrder.jsx";
 import Order, { loader as orderLoader } from "./features/order/Order.jsx";
 import AppLayout from "./ui/AppLayout.jsx";
 import Error from "./ui/Error.jsx";
+import { action as updateOrderAction } from "./features/order/UpdateOrder.jsx";
 
 const router = createBrowserRouter([
   {
@@ -37,6 +40,7 @@ const router = createBrowserRouter([
         element: <Order />,
         loader: orderLoader,
         errorElement: <Error />,
+        action: updateOrderAction,
       },
     ],
   },
